b3ab6fabf46860002f9580eb16b56f7f3195c3e9
vov4k <golovach.volodia@yandex.ru>
2021-05-08 20:22:33 +0300

    Update arduino.ino
    Доделал датчик воды и отправку писем про температуру и влажность

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -6,27 +6,28 @@
 
 
 #define HUMANPIN A0
-#define DHTPIN 2
-//#define HUMANPIN_ 3
 #define SVETPIN A1
+#define WATERPIN A2
+#define DHTPIN 2
+#define BUTTONPIN 3
+//#define HUMANPIN_ 3
 #define SVETPIND 4
 #define OOPIN_ 5
 #define DOPIN_ 6
 #define FLAMEPIN 7
 #define PSPIN_ 8
-#define BUTTONPIN 3
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-#define SS_PIN 10
 #define RST_PIN 9
+#define SS_PIN 10
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
 long minSecsBetweenEmails = 60; // 1 min
-
 unsigned long uidDec, uidDecTemp;  // для храниения номера метки в десятичном формате
 long lastSend = -60000;
-
+long lastSend_klimat = -60000;
 bool ps = false;
 bool rr = false;
 bool sigRFID = false;
+bool sigwater = false;
 bool oo_mode = false;
 bool human = false;
 bool button;
@@ -41,19 +42,19 @@ void setup() {
   Serial.begin(9600);
 
   dht.begin();
-//pinMode(HUMANPIN_, OUTPUT);
+  //pinMode(HUMANPIN_, OUTPUT);
   pinMode(OOPIN_, OUTPUT);
   pinMode(DOPIN_, OUTPUT);
   pinMode(PSPIN_, OUTPUT);
 
   pinMode(DHTPIN, INPUT);
+  pinMode(WATERPIN, INPUT);
   pinMode(SVETPIND, INPUT);
   pinMode(SVETPIN, INPUT);
   pinMode(FLAMEPIN, INPUT);
   pinMode(BUTTONPIN, INPUT);
-  minSecsBetweenEmails = 60; // 1 min
   button = digitalRead(BUTTONPIN);
-  lastSend = -60000;
+
   SPI.begin();  //  инициализация SPI / Init SPI bus.
   mfrc522.PCD_Init();     // инициализация MFRC522 / Init MFRC522 card.
 
@@ -62,12 +63,18 @@ void setup() {
 void loop() {
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
+  long now = millis();
   if (Serial.readString() == "CONFIRM") {
     ps = true;
+    digitalWrite(OOPIN_, HIGH);
+    digitalWrite(DOPIN_, HIGH);
+    digitalWrite(PSPIN_, HIGH);
   }
-  if (Serial.readString() == "KLIMAT") {
-    Serial.println("ANSWERKLIMAT:" + String(temperature)+":"+String(humidity));
+  if (analogRead(WATERPIN) > 200) {
+    sigwater = true;
+    digitalWrite(PSPIN_, HIGH);
   }
+
   if ( mfrc522.PICC_IsNewCardPresent() &&  mfrc522.PICC_ReadCardSerial()) {
     uidDec = 0;
     // Выдача серийного номера метки.
@@ -80,7 +87,7 @@ void loop() {
       sigRFID = true;
       digitalWrite(PSPIN_, HIGH);
       Serial.println("HUMAN");
-    } // Сравниваем Uid метки, если он равен заданому то серва открывает.
+    }
   }
   if (digitalRead(BUTTONPIN) != button) {
     if (oo_mode) {
@@ -89,7 +96,13 @@ void loop() {
       oo_mode = true;
     }
   }
-  long now = millis();
+
+
+  if (ps || rr || sigRFID || sigwater) {
+    return;
+  }
+
+
   if (analogRead(HUMANPIN) > 500) {
     human = true;
     //digitalWrite(HUMANPIN_, HIGH);
@@ -97,23 +110,16 @@ void loop() {
     human = false;
     //digitalWrite(HUMANPIN_, LOW);
   }
-
-  if (ps) {
-    digitalWrite(OOPIN_, HIGH);
-    digitalWrite(DOPIN_, HIGH);
-    digitalWrite(PSPIN_, HIGH);
-    return;
-  }
-  if (rr) {
-    return;
-  }
-  if (sigRFID){
-      return;
-    }
   if (!human) {
     digitalWrite(OOPIN_, LOW);
     digitalWrite(DOPIN_, LOW);
     return;
+  }
+
+
+  if (now > (lastSend_klimat + minSecsBetweenEmails * 1000)) {
+    lastSend_klimat = now;
+    Serial.println("ANSWERKLIMAT:" + String(temperature) + ":" + String(humidity));
   }
   if (oo_mode) {
     analogWrite(OOPIN_, analogRead(SVETPIN) / 4);

95227ffa88aa466a53207cafab0af15b21027177
vov4k <golovach.volodia@yandex.ru>
2021-05-14 00:22:55 +0300

    Описание работы

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,16 +1,15 @@
-
+//Подключение библиотек
 #include "DHT.h"
 #include <Wire.h>
 #include <SPI.h>
 #include <MFRC522.h> // библиотека "RFID".
 
-
+//Обявленеи констант пинов
 #define HUMANPIN A0
 #define SVETPIN A1
 #define WATERPIN A2
 #define DHTPIN 2
 #define BUTTONPIN 3
-//#define HUMANPIN_ 3
 #define SVETPIND 4
 #define OOPIN_ 5
 #define DOPIN_ 6
@@ -18,10 +17,11 @@
 #define PSPIN_ 8
 #define RST_PIN 9
 #define SS_PIN 10
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#define DHTTYPE DHT22
 
-long minSecsBetweenEmails = 60; // 1 min
-unsigned long uidDec, uidDecTemp;  // для храниения номера метки в десятичном формате
+//Обявление переменых нужных для работы
+long minSecsBetweenEmails = 60;
+unsigned long uidDec, uidDecTemp;
 long lastSend = -60000;
 long lastSend_klimat = -60000;
 bool ps = false;
@@ -33,55 +33,75 @@ bool human = false;
 bool button;
 int time_now = 0;
 
+//Обявленеи модулей
 DHT dht(DHTPIN, DHTTYPE);
-
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 void setup() {
-
+  //Инициализация
   Serial.begin(9600);
-
   dht.begin();
-  //pinMode(HUMANPIN_, OUTPUT);
+  SPI.begin();
+  mfrc522.PCD_Init();
+
+  //Режимы пинов
   pinMode(OOPIN_, OUTPUT);
   pinMode(DOPIN_, OUTPUT);
   pinMode(PSPIN_, OUTPUT);
-
   pinMode(DHTPIN, INPUT);
   pinMode(WATERPIN, INPUT);
   pinMode(SVETPIND, INPUT);
   pinMode(SVETPIN, INPUT);
   pinMode(FLAMEPIN, INPUT);
   pinMode(BUTTONPIN, INPUT);
+
   button = digitalRead(BUTTONPIN);
 
-  SPI.begin();  //  инициализация SPI / Init SPI bus.
-  mfrc522.PCD_Init();     // инициализация MFRC522 / Init MFRC522 card.
+
 
 }
 
 void loop() {
 
+  //Проверка активности сигнализации
   if (ps || rr || sigRFID || sigwater) {
     return;
   }
 
-  
+  //Считыванеи температуры, влажности, текущего времени и состояния кнопки для режимов освещения
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
   long now = millis();
+  if (digitalRead(BUTTONPIN) != button) {
+    if (oo_mode) {
+      oo_mode = false;
+    } else {
+      oo_mode = true;
+    }
+  }
+
+  //Активизация пожарной сигнализации, в случае пожара
   if (Serial.readString() == "CONFIRM") {
     ps = true;
     digitalWrite(OOPIN_, HIGH);
     digitalWrite(DOPIN_, HIGH);
     digitalWrite(PSPIN_, HIGH);
   }
+  if (digitalRead(FLAMEPIN) == LOW) {
+    if (now > (lastSend + minSecsBetweenEmails * 1000)) {
+      Serial.println("FLAME:" + String(temperature));
+      lastSend = now;
+    }
+  }
+  
+  //Активизация сигнализации в случае протечки
   if (analogRead(WATERPIN) > 200) {
     sigwater = true;
     Serial.println("WATER");
     digitalWrite(PSPIN_, HIGH);
   }
 
+  //Систма сканирования RFID ключей
   if ( mfrc522.PICC_IsNewCardPresent() &&  mfrc522.PICC_ReadCardSerial()) {
     uidDec = 0;
     // Выдача серийного номера метки.
@@ -96,36 +116,26 @@ void loop() {
       Serial.println("HUMAN");
     }
   }
-  if (digitalRead(BUTTONPIN) != button) {
-    if (oo_mode) {
-      oo_mode = false;
-    } else {
-      oo_mode = true;
-    }
-  }
 
-
-
-
-
+  //Получение информации с датчика движения, в случае отвутствия людей выключаем всё освещение
   if (analogRead(HUMANPIN) > 500) {
     human = true;
-    //digitalWrite(HUMANPIN_, HIGH);
   } else {
     human = false;
-    //digitalWrite(HUMANPIN_, LOW);
   }
-  if (!human) {
+    if (!human) {
     digitalWrite(OOPIN_, LOW);
     digitalWrite(DOPIN_, LOW);
     return;
   }
 
-
+  // Отравка текужей температуры и влажности
   if (now > (lastSend_klimat + minSecsBetweenEmails * 1000)) {
     lastSend_klimat = now;
-    Serial.println("ANSWERKLIMAT:" + String(temperature) + ":" + String(humidity)+":");
+    Serial.println("ANSWERKLIMAT:" + String(temperature) + ":" + String(humidity) + ":");
   }
+
+  //Регулирование освещености по режимам
   if (oo_mode) {
     analogWrite(OOPIN_, analogRead(SVETPIN) / 4);
   } else {
@@ -136,12 +146,7 @@ void loop() {
       digitalWrite(DOPIN_, LOW);
     }
   }
-  if (digitalRead(FLAMEPIN) == LOW) {
-    if (now > (lastSend + minSecsBetweenEmails * 1000)) {
-      Serial.println("FLAME:" + String(temperature));
-      lastSend = now;
-    }
-  }
+
 
   delay(500);
 
